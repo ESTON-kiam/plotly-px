d18f1d2649ab86f4a9cc2e346d97be08236c3010
ESTON-kiam <engestonbrandonkiama@gmail.com>
2024-09-28 13:50:01 +0300

    plotly

---
+++ test.py
@@ -0,0 +1,10 @@
+import plotly.express as px
+
+data={
+    'id':["A","B","C","D","E","F","G"],
+    'parent':["","A","A","B","B","C","D"],
+    'value':[10,10,10,10,10,10,10],
+}
+fig = px.sunburst(data,names='id',parents='parent',values='value')
+fig.update_layout(title_text="Python DRIVERS ME")
+fig.show()
